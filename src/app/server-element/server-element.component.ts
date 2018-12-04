deac7b42f0aee2fe9e7252f2a5ad7fd9521b43aa
ArslanAmeer <arslan.ameer@hotmail.com>
2018-12-04 15:46:19 +0500

    ✨👌 Add: View Encapsulation

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class ServerElementComponent implements OnInit {
   // this is Custom type... -- it is actually a type defination so that element property (typr can onnly have these elements)
