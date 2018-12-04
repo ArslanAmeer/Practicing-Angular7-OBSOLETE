f75d28f9af26105486b8adf300a70349d4a58dd2
ArslanAmeer <arslan.ameer@hotmail.com>
2018-12-04 20:24:33 +0500

    ✔📝 Update: Writing Comments for Better Understanding

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -6,17 +6,21 @@ import { Component, OnInit, EventEmitter, Output, ElementRef, ViewChild } from '
   styleUrls: ['./cockpit.component.css']
 })
 export class CockpitComponent implements OnInit {
+  // Custom Event binding by using @Output() decorator which will emit data through 'serverCreated'
+  // and pass it to server component via app-component , we are sending data high above to app-component
   @Output() serverCreated = new EventEmitter<{
     serverName: string;
     serverContent: string;
   }>();
 
+  // Same Custom Event Binding with an Alias name.
   // tslint:disable-next-line:no-output-rename
   @Output('bpCreated') blueprintCreated = new EventEmitter<{
     serverName: string;
     serverContent: string;
   }>();
 
+  // @ViewChild() is used to get data from its Template DOM using Local Reference
   @ViewChild('serverContentInput') serverContent: ElementRef;
 
   constructor() {}
--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -32,12 +32,19 @@ export class ServerElementComponent
     AfterViewInit,
     AfterViewChecked,
     OnDestroy {
-  // this is Custom type... -- it is actually a type defination so that element property (typr can onnly have these elements)
-  // whereas input is custom property binding decorater
+  // this is Custom type... -- it is actually a type defination so that element property type can only have these elements
+  // - whereas @Input() is custom property binding decorater
   // tslint:disable-next-line:no-input-rename
   @Input('srvElement') element: { type: string; name: string; content: string };
+
+  // We use this name property and bind it because every other component element is rendered through <ng-content>
+  // - which is in app-component
   @Input() name: string;
+
+  // getting data from html template through heading local reference
   @ViewChild('heading') header: ElementRef;
+
+  // getting data of paragraph from Projected content written in app-component
   @ContentChild('contentParagraph') paragraphText: ElementRef;
 
   constructor() {
@@ -51,8 +58,10 @@ export class ServerElementComponent
 
   ngOnInit() {
     console.log('ngonInit Called!');
+
     // You CANNOT have the template content before view init
     console.log('Text Content: ' + this.header.nativeElement.textContent);
+
     // You CANNOT have content data from projected content before Content Init
     console.log('Paragraph Content: ' + this.paragraphText.nativeElement.textContent);
   }
@@ -63,6 +72,7 @@ export class ServerElementComponent
 
   ngAfterContentInit() {
     console.log('ngAfterContentInit Called!');
+
     // You CAN have content data from projected content after Content Init
     console.log('Paragraph Content: ' + this.paragraphText.nativeElement.textContent);
   }
@@ -73,6 +83,7 @@ export class ServerElementComponent
 
   ngAfterViewInit() {
     console.log('ngAfterViewInit Called!');
+
     // You can have the template content after view init
     console.log('Text Content: ' + this.header.nativeElement.textContent);
   }
