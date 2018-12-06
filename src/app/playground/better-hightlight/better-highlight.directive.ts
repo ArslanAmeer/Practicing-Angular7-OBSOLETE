abbd01f8e52b23009f613bdde5864b960c04e1f4
ArslanAmeer <arslan.ameer@hotmail.com>
2018-12-06 20:38:41 +0500

    👌✨ Udpate: Working with @HostBinding in Custom Directive

--- src/app/playground/better-hightlight/better-highlight.directive.ts
+++ src/app/playground/better-hightlight/better-highlight.directive.ts
@@ -1,10 +1,10 @@
-import { Directive, Renderer2, OnInit, ElementRef, HostListener } from '@angular/core';
-import { fail } from 'assert';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
   constructor(private elementref: ElementRef, private renderer: Renderer2) {}
   ngOnInit() {
     // this.renderer.setStyle(this.elementref.nativeElement, 'background-color', 'blue');
@@ -12,10 +12,12 @@ export class BetterHighlightDirective implements OnInit {
 
   @HostListener('mouseenter')
   mouseover(data: Event) {
-    this.renderer.setStyle(this.elementref.nativeElement, 'background-color', 'blue');
+    // this.renderer.setStyle(this.elementref.nativeElement, 'background-color', 'blue');
+    this.backgroundColor = 'blue';
   }
   @HostListener('mouseleave')
   mouseleave(data: Event) {
-    this.renderer.setStyle(this.elementref.nativeElement, 'background-color', 'transparent');
+    // this.renderer.setStyle(this.elementref.nativeElement, 'background-color', 'transparent');
+    this.backgroundColor = 'transparent';
   }
 }
