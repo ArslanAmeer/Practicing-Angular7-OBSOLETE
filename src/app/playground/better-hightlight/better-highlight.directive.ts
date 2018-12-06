eb0d72b6a51013df9e8d03b3286599d9a91c4527
ArslanAmeer <arslan.ameer@hotmail.com>
2018-12-06 23:18:14 +0500

    👌✨ Update: Custom Attribute directive binding

--- src/app/playground/better-hightlight/better-highlight.directive.ts
+++ src/app/playground/better-hightlight/better-highlight.directive.ts
@@ -1,23 +1,28 @@
-import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
-  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
+  // if u enter alias of same name as this drective, you can enter value directly as [driective]="value"
+  @Input('appBetterHighlight') defaultColor: string;
+  @Input() highlightColor: string;
+
+  @HostBinding('style.backgroundColor') backgroundColor: string;
   constructor(private elementref: ElementRef, private renderer: Renderer2) {}
   ngOnInit() {
     // this.renderer.setStyle(this.elementref.nativeElement, 'background-color', 'blue');
+    this.backgroundColor = this.defaultColor; // Setting defualt value on INIT
   }
 
   @HostListener('mouseenter')
   mouseover(data: Event) {
     // this.renderer.setStyle(this.elementref.nativeElement, 'background-color', 'blue');
-    this.backgroundColor = 'blue';
+    this.backgroundColor = this.highlightColor;
   }
   @HostListener('mouseleave')
   mouseleave(data: Event) {
     // this.renderer.setStyle(this.elementref.nativeElement, 'background-color', 'transparent');
-    this.backgroundColor = 'transparent';
+    this.backgroundColor = this.defaultColor;
   }
 }
