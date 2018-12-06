fa30770466fb83751ca4049ba452bff67cb4fcea
ArslanAmeer <arslan.ameer@hotmail.com>
2018-12-06 20:23:16 +0500

    👌✨ Update: Working with @HostListener Custom Directives

--- src/app/playground/better-hightlight/better-highlight.directive.ts
+++ src/app/playground/better-hightlight/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Renderer2, OnInit, ElementRef } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener } from '@angular/core';
 import { fail } from 'assert';
 
 @Directive({
@@ -7,6 +7,15 @@ import { fail } from 'assert';
 export class BetterHighlightDirective implements OnInit {
   constructor(private elementref: ElementRef, private renderer: Renderer2) {}
   ngOnInit() {
+    // this.renderer.setStyle(this.elementref.nativeElement, 'background-color', 'blue');
+  }
+
+  @HostListener('mouseenter')
+  mouseover(data: Event) {
     this.renderer.setStyle(this.elementref.nativeElement, 'background-color', 'blue');
+  }
+  @HostListener('mouseleave')
+  mouseleave(data: Event) {
+    this.renderer.setStyle(this.elementref.nativeElement, 'background-color', 'transparent');
   }
 }
