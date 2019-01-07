4c8d6f6deb46ac73739b02e0eb7279ef49c0c683
ArslanAmeer <arslan.ameer@hotmail.com>
2019-01-08 01:24:55 +0500

    🛠👍 Update: Registered Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CockpitComponent } from './cockpit/cockpit.component';
@@ -20,6 +21,12 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { UsersComponent } from './users/users.component';
 import { UserComponent } from './users/user/user.component';
 
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'servers', component: ServersComponent }
+];
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -38,7 +45,7 @@ import { UserComponent } from './users/user/user.component';
     UsersComponent,
     UserComponent
   ],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
   providers: [AccountsService, LoggingService],
   bootstrap: [AppComponent]
 })
